58b92d6bb4b53361347c43727ac354496015a16d
davidsteinsland <david@davidsteinsland.net>
2013-09-29 12:35:41 +0200

    Removed Ssh package in favor of other package

--- src/Epimetheus/Action/Deploy.php
+++ src/Epimetheus/Action/Deploy.php
@@ -9,52 +9,92 @@ class Deploy extends BaseAction {
 	
 	public function __construct($profile) {
 		$this->_config = $profile;
+	}
+	
+	public function execute() {
+		$configuration = new \Ssh\Configuration($this->_config['address']);
+		$authentication = null;
 		
-		$this->_ssh = new Ssh($this->_config['address']);
-		
-		$authed = false;
 		do {
-			try {
-				$authed = $this->_authenticate();
-			} catch (\Exception $e) {
-				\cli\err($e->getMessage());
+			if (! empty($this->_config['key'])) {
+				$authentication = new \Ssh\Authentication\PublicKeyFile($this->_config['login'], $this->_config['key']['public'],
+					$this->_config['key']['private'], \cli\prompt('Enter passphrase for private key'));
+			} else {
+				$authentication = new \Ssh\Authentication\Password($this->_config['login'], \cli\prompt('Password for user: ' . $this->_config['login']));
 			}
 			
-			if (! $authed) {
+			try {
+				$this->_ssh = new \Ssh\Session($configuration, $authentication);
+				// send a dummy command to spawn RuntimeException if login fails
+				$this->_ssh->getExec()->run('echo Hello > /dev/null');
+				break;
+			} catch (\RuntimeException $e) {
 				if (\cli\choose('Retry') == 'n') {
 					throw new \Exception('Unable to login');
 				}
 			}
-		} while (! $authed);
-	}
-	
-	protected function _authenticate() {
-		if (! empty($this->_config['key'])) {
-			$this->_ssh->auth(Ssh::AUTH_KEY, $this->_config['login'], array(
-				'public'	=> $this->_config['key']['public'],
-				'private'	=> $this->_config['key']['private'],
-				'passphrase'	=> \cli\prompt('Enter passphrase for private key')
-			));
-		} else {
-			$this->_ssh->auth(Ssh::AUTH_PASSWORD, $this->_config['login'], \cli\prompt('Password for user: ' . $this->_config['login']));
-		}
-	}
-	
-	public function execute() {
+		} while (true);
+		
+		\cli\out("Connected to host\n");
+		
 		// run pre-deploy scripts
 		
-		$exists = $this->_ssh->createDirectory($this->_config['path']);
-		$this->_ssh->changeDirectory($this->_config['path']);
-		$repo = \Epimetheus\Configuration::getInstance()['repo'];
-		
-		if ($exists) {
-			$this->_ssh->command('git pull');
-		} else {
-			$this->_ssh->command('git clone git@github.com:' . $repo . '.git . ');
-		}
-		
-		$this->_ssh->command('git checkout ' . $this->_config['branch']);
+		$this->_runScripts('pre-deploy', dirname($this->_config['path']));
+		$this->_deploy();
+		$this->_runScripts('deploy', $this->_config['path']);
 		
 		// run deploy scripts
+	}
+	
+	protected function _runScripts($type, $workingDir = null) {
+		\cli\out('Running ' . $type . ' scripts ... ');
+		$commands = array_merge($this->_config['scripts'][$type], \Epimetheus\Configuration::getInstance()['scripts'][$type]);
+		$exec = $this->_ssh->getExec();
+		
+		foreach ($commands as $command) {
+			\cli\out("\n- " . $command . "\n");
+			
+			if ($workingDir) {
+				$command = 'cd ' . $workingDir . ' && ' . $command;
+			}
+			
+			\cli\out($exec->run($command . "\n"));
+		}
+		
+		\cli\out("Done\n");
+	}
+	
+	protected function _deploy() {
+		$sftp = $this->_ssh->getSftp();
+		$exists = $sftp->exists($this->_config['path']);
+		$dir = dirname ($this->_config['path']);
+		
+		if (! $exists) {
+			\cli\out("Creating directory ...");
+			$sftp->mkdir($dir, 0770, true);
+			\cli\out(" Done\n");
+		}
+		
+		$exec = $this->_ssh->getExec();
+		$exec->run('cd ' . $dir);
+		$repo = \Epimetheus\Configuration::getInstance()['repo'];
+		
+		if (! $exists) {
+			\cli\out("Cloning repository ...");
+			$exec->run('git clone git@github.com:' . $repo . '.git ' . $this->_config['path']);
+			\cli\out(" Done\n");
+		}
+		
+		$exec->run('cd ' . $this->_config['path']);
+		
+		if ($exists) {
+			\cli\out("Pulling from repository ...");
+			$exec->run('git pull');
+			\cli\out(" Done\n");
+		}
+		
+		\cli\out('Checking out ' . $this->_config['branch'] . ' ...');
+		$exec->run('git checkout ' . $this->_config['branch']);
+		\cli\out(" Done\n");
 	}
 }
